d6f6f758762b9a5dc393d751bb9479a0232df06d
Ben Everard <ben@theideabureau.co>
2022-03-08 10:31:27 +0000

    PHPCS fixes

--- src/Navigation.php
+++ src/Navigation.php
@@ -6,172 +6,164 @@ namespace TheIdeaBureau\WordPress\Foundation;
  * A WordPress specific class that bootstraps various useful features
  * for navigation functionality.
  */
-class Navigation {
+class Navigation
+{
+    private static $instance = null;
 
-	private static $instance = null;
+    private function __construct()
+    {
+    }
 
-	private function __construct() {}
+    public static function getInstance()
+    {
 
-	public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new Navigation();
+        }
 
-		if ( self::$instance == null ) {
-			self::$instance = new Navigation();
-		}
+        return self::$instance;
+    }
 
-		return self::$instance;
+    /**
+     * fetches the wordpress menu as a hierarchical array
+     * @param  mixed $name the menu id, slug, name or object
+     * @return array
+     */
+    public function getMenu($name)
+    {
 
-	}
+        // get the menu
+        $menu = wp_get_nav_menu_items($name);
 
-	/**
-	 * fetches the wordpress menu as a hierarchical array
-	 * @param  mixed $name the menu id, slug, name or object
-	 * @return array
-	 */
-	function getMenu($name) {
+        // apply any classes to the menu items
+        _wp_menu_item_classes_by_context($menu);
 
-		// get the menu
-		$menu = wp_get_nav_menu_items($name);
+        // apply hierarchy
+        $new_menu = array();
 
-		// apply any classes to the menu items
-		_wp_menu_item_classes_by_context($menu);
+        foreach ($menu as $menu_item) {
+            $new_menu[$menu_item->ID] = $menu_item;
+            $new_menu[$menu_item->ID]->children = array();
+        }
 
-		// apply hierarchy
-		$new_menu = array();
+        $menu = $new_menu;
 
-		foreach ( $menu as $menu_item ) {
-			$new_menu[$menu_item->ID] = $menu_item;
-			$new_menu[$menu_item->ID]->children = array();
-		}
+        // re-sort by menu order
+        uasort($menu, function ($a, $b) {
 
-		$menu = $new_menu;
+            if ($a->menu_order == $b->menu_order) {
+                return 0;
+            }
 
-		// re-sort by menu order
-		uasort($menu, function($a, $b) {
+            return ($a->menu_order < $b->menu_order) ? -1 : 1;
+        });
 
-		    if ($a->menu_order == $b->menu_order) {
-		        return 0;
-		    }
+        // make the menu hierarchical
+        $menu = $this->buildTree($menu);
 
-		    return ($a->menu_order < $b->menu_order) ? -1 : 1;
+        return $menu;
+    }
 
-		});
+    /**
+     * makes a linear wordpress menu hierarchical
+     * @param  array   $elements the current menu level
+     * @param  integer $parentId the id of the current parent
+     * @return array
+     */
+    private function buildTree(array &$elements, $parentId = 0)
+    {
 
-		// make the menu hierarchical
-		$menu = $this->buildTree($menu);
+        $branch = array();
 
-		return $menu;
+        foreach ($elements as $element) {
+            if ($element->menu_item_parent == $parentId) {
+                $children = $this->buildTree($elements, $element->ID);
 
-	}
+                if ($children) {
+                    $element->children = $children;
+                }
 
-	/**
-	 * makes a linear wordpress menu hierarchical
-	 * @param  array   $elements the current menu level
-	 * @param  integer $parentId the id of the current parent
-	 * @return array
-	 */
-	private function buildTree(array &$elements, $parentId = 0) {
+                $branch[$element->ID] = $element;
+                unset($elements[$element->ID]);
+            }
+        }
 
-		$branch = array();
+        return $branch;
+    }
 
-		foreach ( $elements as $element ) {
+    /**
+     * get breadcrumbs for the current post
+     * @param  array  $options [description]
+     * @return array
+     */
+    public function getBreadcrumbs($options = array())
+    {
 
-			if ( $element->menu_item_parent == $parentId ) {
+        if (is_search()) {
+            return array();
+        }
 
-				$children = $this->buildTree($elements, $element->ID);
+        // create the options object
+        $options = (object) array_merge([
+            'truncate_titles' => true,
+            'truncate_length' => 53,
+            'home_label' => 'Home',
+        ], $options);
 
-				if ( $children ) {
-					$element->children = $children;
-				}
+        global $post;
 
-				$branch[$element->ID] = $element;
-				unset($elements[$element->ID]);
+        // setup
+        $post_type_object = get_post_type_object($post->post_type);
+        $breadcrumbs = array();
 
-			}
+        // post type archive
+        if (is_post_type_archive()) {
+            $breadcrumbs[] = array(get_post_type_archive_link($post_type_object->name) => $post_type_object->label);
+        }
 
-		}
+        // is single or page but not the front-page
+        if (is_single() || ( is_page() && ! is_front_page() )) {
+            $title = get_the_title($post->ID);
 
-		return $branch;
+            if ($options->truncate_titles) {
+                if (strlen($title) > $options->truncate_length) {
+                    $title = trim(substr($title, 0, $options->truncate_length)) . '&hellip;';
+                }
+            }
 
-	}
+            $breadcrumbs[] = array(get_permalink($post->ID) => $title);
+        }
 
-	/**
-	 * get breadcrumbs for the current post
-	 * @param  array  $options [description]
-	 * @return array
-	 */
-	function getBreadcrumbs($options = array()) {
+        // is blog or (specifically) a blog post
+        if (
+            (is_home() || ( is_single() && $post->post_type === 'post'))
+            && $posts_page_id = get_option('page_for_posts')
+        ) {
+            $breadcrumbs[] = array(get_permalink($posts_page_id) => get_the_title($posts_page_id));
+        }
 
-		if ( is_search() ) {
-			return array();
-		}
+        // post parent hierarchy
+        if ($post->post_parent) {
+            $parent_id = $post->post_parent;
 
-		// create the options object
-		$options = (object) array_merge([
-			'truncate_titles' => TRUE,
-			'truncate_length' => 53,
-			'home_label' => 'Home',
-		], $options);
+            while ($parent_id) {
+                $page = get_page($parent_id);
+                $breadcrumbs[] = array(get_permalink($page->ID) => get_the_title($page->ID));
+                $parent_id = $page->post_parent;
+            }
+        }
 
-		global $post;
+        // if this post type has it's own archive, output that
+        if (( is_single() || is_page() ) && $post_type_object->has_archive) {
+            $breadcrumbs[] = array(get_post_type_archive_link($post_type_object->name) => $post_type_object->label);
+        }
 
-		// setup
-		$post_type_object = get_post_type_object($post->post_type);
-		$breadcrumbs = array();
+        // add the top level home link
+        $breadcrumbs[] = array('/' => $options->home_label);
 
-		// post type archive
-		if ( is_post_type_archive() ) {
-			$breadcrumbs[] = array(get_post_type_archive_link($post_type_object->name) => $post_type_object->label);
-		}
+        // remove the 'last' (it's first in the array) breadcrumb link
+        $breadcrumbs[0] = array("" => current($breadcrumbs[0]));
 
-		// is single or page but not the front-page
-		if ( is_single() || ( is_page() && ! is_front_page() ) ) {
-
-			$title = get_the_title($post->ID);
-
-			if ( $options->truncate_titles ) {
-
-				if ( strlen($title) > $options->truncate_length ) {
-					$title = trim(substr($title, 0, $options->truncate_length)) . '&hellip;';
-				}
-
-			}
-
-			$breadcrumbs[] = array(get_permalink($post->ID) => $title);
-		}
-
-		// is blog or (specifically) a blog post
-		if ( ( is_home() || ( is_single() && $post->post_type === 'post' ) ) && $posts_page_id = get_option('page_for_posts') ) {
-			$breadcrumbs[] = array(get_permalink($posts_page_id) => get_the_title($posts_page_id));
-		}
-
-		// post parent hierarchy
-		if ( $post->post_parent ) {
-
-			$parent_id = $post->post_parent;
-
-			while ($parent_id) {
-
-				$page = get_page($parent_id);
-				$breadcrumbs[] = array(get_permalink($page->ID) => get_the_title($page->ID));
-				$parent_id = $page->post_parent;
-
-			}
-
-		}
-
-		// if this post type has it's own archive, output that
-		if ( ( is_single() || is_page() ) && $post_type_object->has_archive ) {
-			$breadcrumbs[] = array(get_post_type_archive_link($post_type_object->name) => $post_type_object->label);
-		}
-
-		// add the top level home link
-		$breadcrumbs[] = array('/' => $options->home_label);
-
-		// remove the 'last' (it's first in the array) breadcrumb link
-		$breadcrumbs[0] = array("" => current($breadcrumbs[0]));
-
-		return array_reverse($breadcrumbs);
-
-	}
-
+        return array_reverse($breadcrumbs);
+    }
 }
--- src/VuePosts.php
+++ src/VuePosts.php
@@ -4,149 +4,130 @@ namespace TheIdeaBureau\WordPress\Foundation;
 
 use TheIdeaBureau\WordPress\Foundation\QueryLoop;
 
-class VuePosts {
+class VuePosts
+{
+    public function __construct($args = array())
+    {
+        $this->args = $args;
+    }
 
-	public function __construct($args = array()) {
-		$this->args = $args;
-	}
+    public function getPosts()
+    {
 
-	public function getPosts() {
+        if (! isset($this->args['ignore'])) {
+            $this->args['ignore'] = array();
+        }
 
-		if ( ! isset($this->args['ignore']) ) {
-			$this->args['ignore'] = array();
-		}
-
-		$query = new QueryLoop($this->args['query']);
-		$vue_posts = [];
-		$post_ids = [];
-
-
-		foreach ( $query as $vue_post ) {
-
-			// track the post ID
-			$post_ids[] = $vue_post->ID;
-
-			$vue_posts[$vue_post->ID] = array(
-				'taxonomies' => array(),
-				'custom' => array(),
-				'display' => true
-			);
-
-			if ( ! in_array('id', $this->args['ignore']) ) {
-				$vue_posts[$vue_post->ID]['id'] = get_the_ID();
-			}
-
-			if ( ! in_array('title', $this->args['ignore']) ) {
-				$vue_posts[$vue_post->ID]['title'] = get_the_title();
-			}
-
-			if ( ! in_array('slug', $this->args['ignore']) ) {
-				$vue_posts[$vue_post->ID]['slug'] = basename(get_the_permalink());
-			}
-
-			if ( ! in_array('post_type', $this->args['ignore']) ) {
-				$vue_posts[$vue_post->ID]['post_type'] = get_post_type();
-			}
-
-			if ( ! in_array('content', $this->args['ignore']) ) {
-				$vue_posts[$vue_post->ID]['content'] = wpautop(get_the_content());
-			}
-
-			if ( ! in_array('link', $this->args['ignore']) ) {
-				$vue_posts[$vue_post->ID]['link'] = get_the_permalink();
-			}
-
-			if ( ! in_array('date', $this->args['ignore']) ) {
-				// $vue_posts[$vue_post->ID]['date'] = OCP::get_the_date();
-			}
+        $query = new QueryLoop($this->args['query']);
+        $vue_posts = [];
+        $post_ids = [];
 
 
-			if ( isset($this->args['taxonomies']) && ! empty($this->args['taxonomies']) ) {
+        foreach ($query as $vue_post) {
+            // track the post ID
+            $post_ids[] = $vue_post->ID;
 
-				// loop through taxonomies
-				foreach ( $this->args['taxonomies'] as $taxonomy ) {
+            $vue_posts[$vue_post->ID] = array(
+                'taxonomies' => array(),
+                'custom' => array(),
+                'display' => true
+            );
 
-					$vue_posts[$vue_post->ID]['taxonomies'][$taxonomy] = array();
+            if (! in_array('id', $this->args['ignore'])) {
+                $vue_posts[$vue_post->ID]['id'] = get_the_ID();
+            }
 
-					if ( $terms = get_the_terms($vue_post->ID, $taxonomy) ) {
+            if (! in_array('title', $this->args['ignore'])) {
+                $vue_posts[$vue_post->ID]['title'] = get_the_title();
+            }
 
-						foreach ( $terms as $term ) {
-							$vue_posts[$vue_post->ID]['taxonomies'][$taxonomy][$term->slug] = $term->name;
-						}
+            if (! in_array('slug', $this->args['ignore'])) {
+                $vue_posts[$vue_post->ID]['slug'] = basename(get_the_permalink());
+            }
 
-					}
+            if (! in_array('post_type', $this->args['ignore'])) {
+                $vue_posts[$vue_post->ID]['post_type'] = get_post_type();
+            }
 
-				}
+            if (! in_array('content', $this->args['ignore'])) {
+                $vue_posts[$vue_post->ID]['content'] = wpautop(get_the_content());
+            }
 
-			}
+            if (! in_array('link', $this->args['ignore'])) {
+                $vue_posts[$vue_post->ID]['link'] = get_the_permalink();
+            }
 
-			if ( isset($this->args['fields']) ) {
+            if (! in_array('date', $this->args['ignore'])) {
+                // $vue_posts[$vue_post->ID]['date'] = OCP::get_the_date();
+            }
 
-				$field_value = function($field, $default_value) {
 
-					// attempt to return the ACF value
-					if ( $value = get_field($field) ) {
+            if (isset($this->args['taxonomies']) && ! empty($this->args['taxonomies'])) {
+                // loop through taxonomies
+                foreach ($this->args['taxonomies'] as $taxonomy) {
+                    $vue_posts[$vue_post->ID]['taxonomies'][$taxonomy] = array();
 
-						// check if the value is an array of WP_Post objects, and attempt to translate the post title
+                    if ($terms = get_the_terms($vue_post->ID, $taxonomy)) {
+                        foreach ($terms as $term) {
+                            $vue_posts[$vue_post->ID]['taxonomies'][$taxonomy][$term->slug] = $term->name;
+                        }
+                    }
+                }
+            }
 
-						if ( is_array($value) ) {
+            if (isset($this->args['fields'])) {
+                $field_value = function ($field, $default_value) {
 
-							foreach ( $value as &$item ) {
+                    // attempt to return the ACF value
+                    if ($value = get_field($field)) {
+                        // check if the value is an array of WP_Post objects, and attempt to translate the post title
 
-								if ( $item instanceof WP_Post ) {
-									$item->post_title = translateFilter($item->post_title, $item->ID, 'title');
-								}
+                        if (is_array($value)) {
+                            foreach ($value as &$item) {
+                                if ($item instanceof WP_Post) {
+                                    $item->post_title = translateFilter($item->post_title, $item->ID, 'title');
+                                }
+                            }
+                        }
 
-							}
+                        return $value;
+                    }
 
-						}
+                    // if we're at this stage the ACF field returned false
+                    // however that might have been a failed ACF field, if
+                    // so return the default value
 
-						return $value;
+                    if (get_post_meta(get_the_ID(), '_' . $field, true)) {
+                        return $default_value;
+                    }
 
-					}
+                    // attempt to return the base meta field value
+                    if ($value = get_post_meta(get_the_ID(), $field, true)) {
+                        return $value;
+                    }
 
-					// if we're at this stage the ACF field returned false
-					// however that might have been a failed ACF field, if
-					// so return the default value
+                    // return the default value as a last resort
+                    return $default_value;
+                };
 
-					if ( get_post_meta(get_the_ID(), '_' . $field, TRUE) ) {
-						return $default_value;
-					}
+                foreach ($this->args['fields'] as $field => $default_value) {
+                    $vue_posts[$vue_post->ID]['fields'][$field] = $field_value($field, $default_value);
+                };
+            }
 
-					// attempt to return the base meta field value
-					if ( $value = get_post_meta(get_the_ID(), $field, TRUE) ) {
-						return $value;
-					}
+            if (isset($this->args['custom'])) {
+                foreach ($this->args['custom'] as $custom => $callback) {
+                    $vue_posts[$vue_post->ID]['custom'][$custom] = $callback($vue_posts[$vue_post->ID], $vue_post);
+                }
+            }
 
-					// return the default value as a last resort
-					return $default_value;
+            if (isset($this->args['postFilter'])) {
+                $vue_posts[$vue_post->ID] = $this->args['postFilter']($vue_posts[$vue_post->ID]);
+            }
+        }
 
-				};
+        $vue_posts = array_values($vue_posts);
 
-				foreach ( $this->args['fields'] as $field => $default_value ) {
-					$vue_posts[$vue_post->ID]['fields'][$field] = $field_value($field, $default_value);
-				};
-
-			}
-
-			if ( isset($this->args['custom']) ) {
-
-				foreach ( $this->args['custom'] as $custom => $callback ) {
-					$vue_posts[$vue_post->ID]['custom'][$custom] = $callback($vue_posts[$vue_post->ID], $vue_post);
-				}
-
-			}
-
-			if ( isset($this->args['postFilter']) ) {
-				$vue_posts[$vue_post->ID] = $this->args['postFilter']($vue_posts[$vue_post->ID]);
-			}
-
-		}
-
-		$vue_posts = array_values($vue_posts);
-
-		return $vue_posts;
-
-	}
-
+        return $vue_posts;
+    }
 }
